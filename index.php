bbecf1053172ae05118d397a1054b5575e10f30c
Kristjan Krips <krips@ut.ee>
2017-02-21 11:22:23 +0200

    Tõlge eesti keelde.

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
   See the License for the specific language governing permissions and
   limitations under the License
 -->
-<html lang="en">
+<html lang="et">
   <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -38,14 +38,14 @@
         <div class="demo-container mdl-grid">
           <div class="mdl-cell mdl-cell--2-col mdl-cell--hide-tablet mdl-cell--hide-phone"></div>
           <div class="demo-content mdl-color--white mdl-shadow--4dp content mdl-color-text--grey-800 mdl-cell mdl-cell--8-col">
-            <h3>Cookies</h3>
+            <h3>Küpsised</h3>
             <p>
-              Welcome! Visiting this site is part of a homework for the Information Security course.
+              Tere! Selle veebilehe külastamine on osa infoturbe aine kodutööst.
             </p>
             <p>
-              There is a HTTP cookie named <tt>infsec_secret</tt> in your web browser with a randomly generated value (32 symbols).
-              Please copy that value into the form provided below, together with your name and student ID.
-              The score table will be updated once the task deadline has passed.
+		      Sinu veebibrauserisse on lisatud küpsis nimega <tt>infoturbe_saladus</tt>. Antud küpsise sisuks on juhuslikult genereeritud 32 sümboli pikkune väärtus.
+		      Leidke kirjeldatud küpsis ning kopeerige selle väärtus allpool olevasse vormi. Enne ülesande esitamist tuleb sisestada ka enda nimi ja matriklinumber.
+		      Tulemust näete hinnete tabelist pärast kodutöö tähtaja möödumist.
             </p>
 <?php
   session_start();
@@ -55,7 +55,7 @@
     $bytes = function_exists('random_bytes') ? random_bytes(16) : openssl_random_pseudo_bytes(16);
     $secret = bin2hex($bytes);
     $_SESSION['secret'] = $secret;
-    setcookie('infsec_secret', $secret);
+    setcookie('infoturbe_saladus', $secret);
   }
 
   // Parse submission
@@ -70,14 +70,14 @@
       fclose($f);
       $_SESSION['ok'] = true;
     } else {
-      $_SESSION['submit-message'] = 'Sorry, but the secret value you provided does not match the one we created. Please try agin.';
+      $_SESSION['submit-message'] = 'Vabandust, aga esitatud küpsise väärtus ei ole see, mis oli meie poolt loodud. Palun proovi uuesti.';
     }
   }
 
   if (isset($_SESSION['ok'])) {
 ?>
                   <div class="alert alert-success" role="alert">
-                    Correct. Thank you.
+                    Õige vastus!
                   </div>
 <?php
   } else if (isset($_SESSION['error-message'])) {
@@ -100,19 +100,19 @@
               <form action="#" method="post">
                   <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
                       <input class="mdl-textfield__input" type="text" id="Name" name="Name">
-                      <label class="mdl-textfield__label" for="Name">Full name...</label>
+                      <label class="mdl-textfield__label" for="Name">Täisnimi...</label>
                   </div>
                   <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
                       <input class="mdl-textfield__input" type="text" id="StudentID" name="StudentID">
-                      <label class="mdl-textfield__label" for="StudentID">Student ID... (e.g. A12345)</label>
+                      <label class="mdl-textfield__label" for="StudentID">Matriklinumber...</label>
                   </div>
                   <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
                       <input class="mdl-textfield__input" type="text" id="Secret" name="Secret">
-                      <label class="mdl-textfield__label" for="Secret">Secret value from cookie...</label>
+                      <label class="mdl-textfield__label" for="Secret">Küpsise väärtus...</label>
                   </div>
                   <p>
                       <button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent" type="submit" name="submit">
-                          Submit
+                          Esita lahendus
                       </button>
                   </p>
               </form>
